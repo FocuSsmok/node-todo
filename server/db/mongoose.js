4db70e51b9534fd4e19ebbacf1f846b4a4ed0066
Patryk Przebirowski <przebirowski.p@gmail.com>
2018-10-25 17:18:10 +0200

    Setup test db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true});
+mongoose.connect(process.env.MONODB_URI, { useNewUrlParser: true});
 
 module.exports = { mongoose };
+
+// process.env.NODE_ENV == 'production'
--- server/server.js
+++ server/server.js
@@ -1,3 +1,15 @@
+var env = process.env.NODE_ENV || 'development';
+
+console.log('env: ', env);
+
+if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
 const lodash = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
