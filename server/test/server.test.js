4701b0017577f99cba06e9f04a5708f3c126c1af
Patryk Przebirowski <przebirowski.p@gmail.com>
2018-10-25 15:42:29 +0200

    Add update route

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -112,3 +112,41 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+
+        var id = todos[1]._id.toHexString();
+
+        request(app)
+            .delete(`/todos/${id}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo.text).toBe(todos[1].text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.findById(id).then((todo) => {
+                    // expect(todo).toNotExist(); nie działa :(
+                    done();
+                }).catch(e => done(e));
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        request(app)
+        .delete(`/todos/${new ObjectID().toHexString}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+        .delete(`/todos/1321312`)
+        .expect(404)
+        .end(done);
+    });
+
+});
